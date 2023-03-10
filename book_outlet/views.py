7fc89120417e9169ab7a7a3802cb4a4899b5a86f
wamah <pythondeveloper21@gmail.com>
2023-03-10 04:51:27 +0000

    quering data from the database

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from .models import Book
+
 # Create your views here.
+
+def index(request):
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.htm", {"books":books})
+
+def book_detail(request, id):
+    book = Book.objects.get(id=id)
+    return render(request, "book_detail.htm", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestseller,
+        })
