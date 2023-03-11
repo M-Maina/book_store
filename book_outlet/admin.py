4d7b365b26dff46149a1b3981ccbf5e8d6a47eeb
wamah <pythondeveloper21@gmail.com>
2023-03-11 17:25:11 +0000

    added address model

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Address
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -10,3 +10,5 @@ class BookAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,11 +5,36 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return '%s, %s, %s' % (self.street, self.postal_code, self.city)
+     
+    class Meta:
+        verbose_name_plural = "Address Entries"
+    
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+    
+    
+    def full_name(self):
+        return '%s %s' % (self.first_name, self.last_name) #we can use this method inside templates if needed
+    #f"{self.first_name} {self.last_name}"
+    
+    
+    def __str__(self):
+        return self.full_name()
+    
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)#database make it easier to be found
     #blank = True, editable = False tells the database that this field can be saved empty/ not editable
